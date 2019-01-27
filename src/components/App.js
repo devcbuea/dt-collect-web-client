c317c8a4fb4325a45c42a31d7e0e114dbf499c47
Crespy <cressencey@gmail.com>
2019-01-27 10:55:20 +0100

    File structure

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './../../assets/styles/Appcss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>DevC web app</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
