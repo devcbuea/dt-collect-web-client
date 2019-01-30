ecef48a13aa0bb2e31199e0fba4abea0fa822a7d
DanCarl857 <dancarl758@gmail.com>
2019-01-30 23:52:29 +0100

    first commit

---
+++ src/reducers/reducers.js
@@ -0,0 +1,21 @@
+let defaultState = {
+    examplePropOne: 'example',
+    examplePropTwo: 'test'
+};
+
+const reducers = (state = defaultState, action ) => {
+    switch (action.type) {
+        case 'EXAMPLE' :
+        return {
+            ...state,
+            examplePropOne: 'newPropOne'
+        }
+        case 'EXAMPLE-TWO':
+          return {
+              ...state,
+              examplePropTwo: action.payload
+          }
+          default: return state;
+    }
+}
+export default reducers;
