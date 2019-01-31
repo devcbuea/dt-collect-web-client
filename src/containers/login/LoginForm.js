01409db72cdd21c2ea6f0f076c8b3f75142790e1
Crespy <cressencey@gmail.com>
2019-01-31 13:23:27 +0100

    Restructure login form and remove butoon outline

--- src/containers/login/LoginForm.js
+++ src/containers/login/LoginForm.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import { FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const LoginForm = () => (                              
+const LoginForm = () => (
      <div className="login-form-section">
           <h2>Login</h2>
           <form>
                <FormGroup controlId="email-group" validationState="success">
                     <ControlLabel>Username or Email</ControlLabel>
-                    <FormControl type="text" 
-                         placeholder=""/>
+                    <FormControl type="text" placeholder="" />
                </FormGroup>
                <FormGroup controlId="email-group" validationState="success">
                     <ControlLabel>Password</ControlLabel>
-                    <FormControl type="password" 
-                         placeholder=""/>
+                    <FormControl type="password" placeholder="" />
                </FormGroup>
                <Button className="login-devc">Login</Button>
                <div className="pass-reset">
                     <div className="new-account-link">
-                         <p>New here? <a href="#"> Create an account</a></p> 
+                         <p>New here? <Link to="/"> Create an account</Link>  |</p>
                     </div>
                     <div className="forgot-pass">
-                         <a href="#"> Forgot Password</a>
+                         <Link to="/"> Forgot Password</Link>
                     </div>
                </div>
                <p className="alt-login">-OR-</p>
