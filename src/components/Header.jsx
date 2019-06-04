0a2e5064753f7d2d8a6aa5e30a51f1e15c634d41
arnol <arnolfokam23@gmail.com>
2019-06-04 15:05:33 +0100

    header completion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,40 @@ import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 import './../assets/styles/header.css';
 
-const Header = () => {
-  return (
-    <Navbar collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          Data Collection Tool
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: false };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            DEVC<br/>BUEA
         </Navbar.Brand>
-      </Navbar.Header>
-      <Nav pullRight>
-        <NavItem> Link One </NavItem>
-        <NavItem> Link Two </NavItem>
-        <NavItem> Link Three </NavItem>
-      </Nav>
-    </Navbar>
-  );
-};
+        </Navbar.Header>
+        <Nav pullRight className={this.state.isToggleOn ? 'on' : ''}>
+          <NavItem>Home</NavItem>
+          <NavItem>Create Account</NavItem>
+          <NavItem>About us</NavItem>
+          <NavItem>Conctact us</NavItem>
+        </Nav>
+        <div className="nav-toggle" onClick={this.handleClick}>
+          <div className={"hamburger " + (this.state.isToggleOn ? 'on' : '')}></div>
+        </div>
+      </Navbar>
+    );
+  }
+}
 export default Header;
